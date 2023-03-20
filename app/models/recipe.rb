f69cc9baf7fda5f9c9724fec2985668e64ce690f
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-20 17:45:59 +0200

    Add Numericality validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
 
   validates :name, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
+  validates :preparation_time, presence: true, numericality: { only_inter: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { only_inter: true, greater_than_or_equal_to: 0 }
   validates :description, presence: true
 end
