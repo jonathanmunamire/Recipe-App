2c283eca0471bc88321e8f7e01cd87dcf808d42f
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-21 14:04:05 +0200

    Retrieve DB data

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @foods = current_user.foods.all
+    # @foods = Food.find_by(user: params[:current_user.id])
   end
 end
