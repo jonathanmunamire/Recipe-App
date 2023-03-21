c46e6152a0a47d6ac1a78a2499f2174109b86a9c
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-21 15:06:45 +0200

    Add Forms

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,4 +3,22 @@ class FoodsController < ApplicationController
     @foods = current_user.foods.all
     # @foods = Food.find_by(user: params[:current_user.id])
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.create(params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id))
+
+    respond_to do |format|
+      format.html do
+        if @food.save
+          redirect_to foods_path
+        else
+          redirect_to new_food_path
+        end
+      end
+    end
+  end
 end
