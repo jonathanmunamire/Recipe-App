0fae2746bd0bc6e84ecc115363f72fa801de1b41
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-22 16:56:27 +0200

    Add display order

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
     # @recipes = Recipe.where(public: true)
-    @recipes = Recipe.where(public: false)
+    @recipes = Recipe.where(public: false).order(created_at: :desc)
   end
 end
