a2d9869f1a785c959d398b799c6c20b6da3c0a97
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-22 17:09:40 +0200

    Update Public Controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
     # @recipes = Recipe.where(public: true)
-    @recipes = Recipe.where(public: false).order(created_at: :desc)
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
