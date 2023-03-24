4a7f11d2d318201d8caf009ee9170a62148f9a13
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-24 14:21:22 +0200

    Implement shopping List methods

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,4 +1,16 @@
 class ShoppingListsController < ApplicationController
   def index
+    @recipes = current_user.recipes
+    @total_missing_count = 0
+    @total_missing_price = 0
+    @missing_foods = []
+    @recipe_foods = RecipeFood.all
+
+    @recipe_foods.each do |recipe_food|
+      missing_quantity = recipe_food.quantity - recipe_food.food.quantity
+      @missing_foods << recipe_food
+      @total_missing_count += 1
+      @total_missing_price += missing_quantity * recipe_food.food.price
+    end
   end
 end
