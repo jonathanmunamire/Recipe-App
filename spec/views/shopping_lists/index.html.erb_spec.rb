d9e2f9f4e1e45752bbb03dbd7041e1112be75250
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-24 23:22:25 +0200

    Add Tests

--- spec/views/shopping_lists/index.html.erb_spec.rb
+++ spec/views/shopping_lists/index.html.erb_spec.rb
@@ -4,15 +4,14 @@ RSpec.feature 'Shopping index page', type: :feature do
   let(:user) { FactoryBot.create(:user) }
 
   before do
-    # sign_in user
+    sign_in user
     visit shopping_lists_path
   end
 
   scenario 'Displays details general shopping list' do
     expect(page).to have_content('Shopping List')
     expect(page).to have_content('Amount of food items to buy: 0')
-    expect(page).to have_content('Total value of food: $0')
-    expect(page).to have_content('Food')
-    expect(page).to have_content('Back to recipe')
+    expect(page).to have_content('Total value of food needed: $0')
+    expect(page).to have_content('No Shopping List')
   end
 end
